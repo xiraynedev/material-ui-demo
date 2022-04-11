b3d2942c461345f4e89bf0120646c053454c1543
xiraynedev <xiraynedev@gmail.com>
2022-04-11 19:21:37 -0400

    update MuiSkeleton component

--- components/MuiSkeleton.tsx
+++ components/MuiSkeleton.tsx
@@ -1,16 +1,26 @@
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function MuiSkeleton() {
   
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000)
+  }, [])
+
   return (
     <Stack marginY={4} width={250}>
       {
-          <h1>{loading ? <Skeleton /> : 'Ready to go!' }</h1>
-      }      
+        loading ? (
+          <Skeleton variant='rectangular' width={250} height={250} />
+        ) : (
+          <img src='https://source.unsplash.com/random/250x250' alt='random image' />
+        )
+      }
     </Stack>
   );
 }
